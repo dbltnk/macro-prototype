2137f93ddf4ce0618148780c90b5def1c1a5495c
Alexander Zacherl <alexander.zacherl@googlemail.com>
2013-07-22 15:15:04 +0200

    random start locations

--- Clan.lua
+++ Clan.lua
@@ -22,31 +22,28 @@ Clan = Sprite:extend
 		the.app.view.layers.management:add(self)
 		the.clans[self] = true
 		self.name = localconfig.team 
-		self.hometown = the["spawnpoint" .. math.random(1,4)]
 		self.color = {math.random(0,255),math.random(0,255),math.random(0,255)}
-		if self.hometown then
-			local homeX, homeY = tools.object_center(self.hometown)
-			if not self.created_via_network then		
-				self.testChar1 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 2, XPLevel = 0, equipLevel = 0, loginTime = 16, logoutTime = 22}
-				table.insert(self.members, self.testChar1)
-				self.testChar1.clan = self.name
-				self.testChar1.nr = 1
+		local homeX, homeY = math.random(200,800), math.random(200,800)
+		if not self.created_via_network then		
+			self.testChar1 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 2, XPLevel = 0, equipLevel = 0, loginTime = 16, logoutTime = 22}
+			table.insert(self.members, self.testChar1)
+			self.testChar1.clan = self.name
+			self.testChar1.nr = 1
+		
+			self.testChar2 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 1, XPLevel = 0, equipLevel = 0, loginTime = 12, logoutTime = 23}
+			table.insert(self.members, self.testChar2)
+			self.testChar2.clan = self.name
+			self.testChar2.nr = 2
 			
-				self.testChar2 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 1, XPLevel = 0, equipLevel = 0, loginTime = 12, logoutTime = 23}
-				table.insert(self.members, self.testChar2)
-				self.testChar2.clan = self.name
-				self.testChar2.nr = 2
-				
-				self.testChar3 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 1, XPLevel = 0, equipLevel = 0, loginTime = 20, logoutTime = 15}	
-				table.insert(self.members, self.testChar3)
-				self.testChar3.clan = self.name
-				self.testChar3.nr = 3
-				
-				self.testChar4 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 3, XPLevel = 0, equipLevel = 0, loginTime = 18, logoutTime = 15}		
-				table.insert(self.members, self.testChar4)
-				self.testChar4.clan = self.name
-				self.testChar4.nr = 4
-			end
+			self.testChar3 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 1, XPLevel = 0, equipLevel = 0, loginTime = 20, logoutTime = 15}	
+			table.insert(self.members, self.testChar3)
+			self.testChar3.clan = self.name
+			self.testChar3.nr = 3
+			
+			self.testChar4 = Character:new{x= homeX + math.random(-50,50),y = homeY + math.random(-50,50), skillLevel = 3, XPLevel = 0, equipLevel = 0, loginTime = 18, logoutTime = 15}		
+			table.insert(self.members, self.testChar4)
+			self.testChar4.clan = self.name
+			self.testChar4.nr = 4
 		end
 	end,
 	
